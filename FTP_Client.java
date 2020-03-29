6e394d1d457b52490335fa3f24645517c1b99212
MiguelGraciaCarballo <59137643+MiguelGraciaCarballo@users.noreply.github.com>
2020-03-29 17:29:17 +0200

    Update FTP_Client.java
    Now, it has a menu which displays the different options and sends the one selected by the user to the server.

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,7 +1,31 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class FTP_Client {
+	
+	public enum Options {
+		
+		LIST( "List all files on a directory" ),
+		RETR( "Download a file from server" ),
+		STOR( "Upload a file to the server" ),
+		PWD( "Get the path to the working directory" ),
+		CWD( "Change working directory" ),
+		MKD( "Create directory" ),
+		DELE( "Remove directory" ), 
+		RNFR( "Delete a file" ),
+		QUIT( "Exit" );
+		
+		private final String description;
+		
+		Options( String description ) {
+			this.description = description;
+		}
+		
+		public String getDescription() {
+			return description;
+		}
+	};
 
     public static void main(String args[]) {
         int commandPort = 21;
@@ -13,6 +37,9 @@ public class FTP_Client {
         String data;
         String result;
         boolean connection = true;
+        
+        String option;
+        
 
         try {
             // Connect to the server
@@ -27,10 +54,16 @@ public class FTP_Client {
                 // Get text from the keyboard
                 inputKeyboard = new BufferedReader(new InputStreamReader(System.in));
                 //System.out.print("Write text (END to close the server): ");
-                data = inputKeyboard.readLine();
+                
+                printMenu();
+                option = getOption(inputKeyboard);
+                
+                
+                //data = inputKeyboard.readLine();
 
                 // Send data to the server
-                output.println(data);
+                //output.println(data);
+                output.println(option);
                 // Read data from the server
                 result = input.readLine();
 
@@ -49,4 +82,49 @@ public class FTP_Client {
             System.out.println("Error: " + e);
         }
     } // main
+    
+    /**
+     * Loops the enumerator and prints a menu with the options
+     */
+    private static void printMenu() {
+    	
+    	System.out.println( "This are the options");
+    	
+    	for ( Options options : Options.values() ) {
+    		  System.out.println( "\t(" + options.ordinal() + ") " + options.description );
+    	}
+    } //printMenu
+    
+    /**
+     * Ask for an option, checks if it is a valid one, and returns the name of the enumerator
+     * 
+     * @return command 
+     */
+    private static String getOption(BufferedReader inputKeyboard) {
+    	
+    	int option = Options.values().length; //This will avoid get an option in case the input is a string
+    	String command = "";
+    	
+    	do {
+    		System.out.println( "Which option do you want to select: " );
+    		try {
+				option = Integer.parseInt(inputKeyboard.readLine());
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		if( option < 0 || option > Options.values().length - 1) {
+    			System.out.println( "That option is not implemented." );
+    		}
+    	}while(option < 0 || option > Options.values().length - 1);  
+    	
+    	command = Options.values()[option].name();
+    	System.out.println( "You have choose to: " + Options.values()[option].description );
+    	
+    	return command;
+    }
+    
 } // class CharacterClient
