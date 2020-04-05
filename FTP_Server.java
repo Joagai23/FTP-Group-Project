fdd4296a2e9828918833045cd9d7659a18fc1e6b
Jorge Aguirregomezcorta <jaguirregomezcorta@gmail.com>
2020-04-05 16:39:29 +0200

    Update FTP_Server.java

--- FTP_Server.java
+++ FTP_Server.java
@@ -1,21 +1,17 @@
-import java.net.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.io.*;
 
 public class FTP_Server {
-	
-	private static String logFile = "serverLog.txt";
+
+    private static String logFile = "serverLog.txt";
 
     public static void main(String args[]) {
 
         Server server = new Server();
         ServerOptions serverOptions = new ServerOptions(server);
-        boolean serverControl = true;
+        boolean hasDataPortNumber = false;
         boolean connectionControl = false;
-        
         //BufferedReader input;
         //PrintWriter output;
 
@@ -27,26 +23,36 @@ public class FTP_Server {
             {
                 System.out.println("Character Server waiting for requests");
                 // Accept a connection  and create the socket for the transmission with the client
-                server.connectionSocket = server.serverConnectionSocket.accept();
+                server.commandSocket = server.serverConnectionSocket.accept();
                 connectionControl = true;
                 System.out.println("Connection accepted");
-				server.CreateWritersReaders();
+				server.createCommandWritersReaders();
+
+				int dataPort = -1;
+
+				// Get data port number from the client and update value in server
+				while(dataPort == -1)
+                {
+                    //put time out
+                    dataPort = Integer.parseInt(server.getInputCommandSocket().readLine());
+                }
+
+				server.setDataPort(dataPort);
 
                 while(connectionControl)
                 {
                     // Read the data sent by the client
-                    data =  server.getInputConnectionSocket().readLine();
+                    data =  server.getInputCommandSocket().readLine();
                     System.out.println("Server receives: " + data);
                     // Convert text to Upper Case
                     //data = data.toUpperCase();
                     // Send the text
                     //data = "220 Service ready for new user";
                     serverOptions.readOption(data);
-                    
-                    
+
                     registerAction("test", data);
-                    
-                    server.getOutputConnectionSocket().println(data);
+
+                    server.getOutputCommandSocket().println(data);
                     
                     System.out.println("Server sends: " + data);
 
@@ -57,7 +63,7 @@ public class FTP_Server {
                 }
 
                 // Close the socket
-                server.connectionSocket.close();
+                server.commandSocket.close();
 
             } catch(IOException e)
             {
@@ -67,27 +73,25 @@ public class FTP_Server {
         // Close the server socket
         //server.getServerConnectionSocket().close();
     } // main
-    
-	
-	public static void registerAction(String user, String command) {
-	    
-	    LocalDateTime date = LocalDateTime.now();
-	    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-	    String formattedDate = date.format(dateFormat);
-	    
-	    try {
-			BufferedWriter fileWriter = new BufferedWriter(new FileWriter(logFile, true));
-			
-			fileWriter.newLine();
-			fileWriter.write("User: " + user + " - " + formattedDate + " - " + command);
-			
-			fileWriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	}
-    
-    
+    public static void registerAction(String user, String command) {
+
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = date.format(dateFormat);
+
+        try {
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(logFile, true));
+
+            fileWriter.newLine();
+            fileWriter.write("User: " + user + " - " + formattedDate + " - " + command);
+
+            fileWriter.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 } // class CharacterServer
