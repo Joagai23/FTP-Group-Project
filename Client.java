8f8599da3471185935692856d3e67c8fb05c4ba7
Jorge Aguirregomezcorta <jaguirregomezcorta@gmail.com>
2020-04-05 16:38:42 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -8,11 +8,11 @@ import java.net.Socket;
 
 public class Client {
 
-    private int connectionPort = 0;
+    private int commandPort = 0;
     private final int dataPort = 22;
     private String configFile = "config.txt";
     private final String IP = "localhost";
-    private Socket connectionSocket;
+    private Socket commandSocket;
     private Socket dataSocket;
     private BufferedReader inputConnectionSocket;
     private PrintWriter outputConnectionSocket;
@@ -23,19 +23,18 @@ public class Client {
 
     public Client()
     {
-    	
-    	getPortFromConfigFile();
-    	establishConnection();
-    	CreateWritersReaders();
-    	
+        setPortFromConfigFile();
+        establishConnection();
+        createCommandWritersReaders();
+        sendDataPortNumber();
+
+
     	/*
         try {
             // Establish connection with Server via connection socket
-            connectionSocket = new Socket("localhost", 21);
-
-            CreateWritersReaders();
+            commandSocket = new Socket("localhost", 21);
+            createCommandWritersReaders();
             // Send socket port number for data socket
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,9 +43,9 @@ public class Client {
 
     /***************************************************************/
 
-    public int getConnectionPort()
+    public int getCommandPort()
     {
-        return connectionPort;
+        return commandPort;
     }
 
     public int getDataPort()
@@ -54,14 +53,14 @@ public class Client {
         return dataPort;
     }
 
-    public void setConnectionSocket(Socket connectionSocket)
+    public void setCommandSocket(Socket commandSocket)
     {
-        this.connectionSocket = connectionSocket;
+        this.commandSocket = commandSocket;
     }
 
-    public Socket getConnectionSocket()
+    public Socket getCommandSocket()
     {
-        return connectionSocket;
+        return commandSocket;
     }
 
     public void setDataSocket(Socket dataSocket)
@@ -96,42 +95,47 @@ public class Client {
 
     /***************************************************************/
 
-    private void CreateWritersReaders()
+    private void createCommandWritersReaders()
     {
         // Get the inputConnectionSocket/outputConnectionSocket from the socket
         try {
-            inputConnectionSocket = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            outputConnectionSocket = new PrintWriter(connectionSocket.getOutputStream(), true);
+            inputConnectionSocket = new BufferedReader(new InputStreamReader(commandSocket.getInputStream()));
+            outputConnectionSocket = new PrintWriter(commandSocket.getOutputStream(), true);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    private void getPortFromConfigFile() {
-    	
-    	try {
-			BufferedReader fileReader = new BufferedReader(new FileReader(configFile));
-			try {
-				connectionPort = Integer.parseInt(fileReader.readLine());
-				System.out.println(connectionPort);
-				fileReader.close();
-			} catch (NumberFormatException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+    private void setPortFromConfigFile() {
+
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader(configFile));
+            try {
+                commandPort = Integer.parseInt(fileReader.readLine());
+                System.out.println(commandPort);
+                fileReader.close();
+            } catch (NumberFormatException | IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-    
+
     private void establishConnection() {
-    	
-    	try {
-			connectionSocket = new Socket(IP, connectionPort);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+        try {
+            commandSocket = new Socket(IP, commandPort);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void sendDataPortNumber()
+    {
+        outputConnectionSocket.println(dataPort);
     }
 }
