99bb3060aae96fb6bc610eff0072818813a76788
MiguelGraciaCarballo <59137643+MiguelGraciaCarballo@users.noreply.github.com>
2020-04-19 13:25:18 +0200

    Update FTP_Client.java

--- FTP_Client.java
+++ FTP_Client.java
@@ -7,19 +7,18 @@ public class FTP_Client {
     public static void main(String args[]) {
 
 		Client client = new Client();
-		ClientOptions clientOptions = new ClientOptions();
 
         String result;
 		String option;
 		String command;
-		String path;
+		String path = "";
 		boolean connection = true;
 
         try {
 
 			String dataPortResponse = client.getInputConnectionSocket().readLine();
 			System.out.println(dataPortResponse);
-			if(dataPortResponse.compareTo("OK") != 0)
+			if(!dataPortResponse.contains("220"))
 			{
 				connection = false;
 			}
@@ -29,10 +28,13 @@ public class FTP_Client {
                 // Get text from the keyboard
                 inputKeyboard = new BufferedReader(new InputStreamReader(System.in));
 
-				clientOptions.printMenu();
-                option = clientOptions.getOption(inputKeyboard);
-				path = clientOptions.getPath(inputKeyboard);
-				command = clientOptions.getCommand(option, path);
+				ClientOptions.printMenu();
+                option = ClientOptions.getOption(inputKeyboard);
+                
+                if(ClientOptions.needPath(option)) {
+                	path = ClientOptions.getPath(inputKeyboard);
+                }
+				command = ClientOptions.getCommand(option, path);
 
                 // Send data to the server
                 client.getOutputConnectionSocket().println(command);
