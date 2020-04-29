d54c3d5f1747c9568f1fe0ee68ebe88ab5623d9f
Jorge Aguirregomezcorta <jaguirregomezcorta@gmail.com>
2020-04-29 16:57:39 +0200

    Update ClientOptions.java

--- ClientOptions.java
+++ ClientOptions.java
@@ -53,7 +53,7 @@ public class ClientOptions {
 				System.out.println(this.name());
 			}
 		},
-        RNFR( "Delete a file", true ) {
+        RNFR( "Rename a file", true ) {
 			@Override
 			public void doWork() {
 				System.out.println(this.name());
@@ -86,7 +86,7 @@ public class ClientOptions {
      */
     public static void printMenu() {
 
-        System.out.println( "This are the options");
+        System.out.println( "These are the options");
 
         for ( Options options : Options.values() ) {
             System.out.println( "\t(" + options.ordinal() + ") " + options.description );
@@ -120,7 +120,7 @@ public class ClientOptions {
         }while(option < 0 || option > Options.values().length - 1);
 
         command = Options.values()[option].name();
-        System.out.println( "You have choose to: " + Options.values()[option].description );
+        System.out.println( "You have chosen to: " + Options.values()[option].description );
 
         return command;
     }
@@ -190,4 +190,74 @@ public class ClientOptions {
     	return needPath;
     }
 
+    // Returns 0 if command doesn't require data, 1 if requires character mode data connection and 2 if byte data connection
+    public static int requiresDataConnection(String option){
+
+        if(option.compareTo("LIST") == 0){
+            return  1;
+        }else if(option.compareTo("RETR") == 0 || option.compareTo("STOR") == 0){
+            return 2;
+        }
+
+        return 0;
+    }
+
+    public static String FTPClient(Client client, String option, String path) {
+
+        try{
+
+            // Check if command requires data connection and send PORT command if so
+            int requiresDataConnection = requiresDataConnection(option);
+            if(requiresDataConnection != 0) {
+                String portCommand = client.sendPort();
+
+                // Send port command and wait for response from server
+                client.getOutputConnectionSocket().println(portCommand);
+                String portResponse = client.getInputConnectionSocket().readLine();
+
+                System.out.println("Response from port: " + portResponse);
+
+                if (portResponse.contains("503")) { // Bad sequence of commands.
+                    return portResponse;
+                }
+            }
+
+            // Create command to send to server
+            String command = getCommand(option, path);
+            System.out.println(command);
+
+            // Send data to the server
+            client.getOutputConnectionSocket().println(command);
+            // Read data from the server
+            String response = client.getInputConnectionSocket().readLine();
+
+            if(!response.contains("150")){ // Doesn't need to open data connection
+                return response;
+            }
+
+            // Open data connection
+            client.openDataSocket();
+            client.createDataReader(requiresDataConnection);
+
+            // Wait for next command and close connection
+            /*do{
+
+                System.out.println("C: " + response);
+                System.out.println("D: " + data);
+            }while(!response.contains("226") || !response.contains("451") || !response.contains("425") || !response.contains("426"));*/
+
+            String data = client.readData(requiresDataConnection);
+            response = client.getInputConnectionSocket().readLine();
+            System.out.println("D: " + data);
+
+            client.closeDataSocket();
+            client.getOutputConnectionSocket().println("CLDT");
+            return response;
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "QUIT";
+    }
 }
